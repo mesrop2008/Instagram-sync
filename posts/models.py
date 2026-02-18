06bb4703e6cf509487a919619955ff0a1a93f640
mesrop2008 <mesropharutyunyan622@gmail.com>
2026-02-18 22:23:47 +0800

    create  models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Post(models.Model):
+    id = models.CharField(max_length=50, unique=True)
+    media_type = models.CharField(max_length=30)
+    media_url = models.URLField()
+    perma_link = models.URLField()
+    timestamp = models.DateTimeField()
 
+
+class Comment(models.model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
